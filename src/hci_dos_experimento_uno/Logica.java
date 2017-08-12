0c6998796e481e42efbced4e9833887210794d49
Jose David Giraldo <josedavidgm1995@gmail.com>
2017-08-12 11:13:43 -0500

    Agregué pasto :v

--- src/hci_dos_experimento_uno/Logica.java
+++ src/hci_dos_experimento_uno/Logica.java
@@ -4,25 +4,22 @@ import java.util.LinkedList;
 import java.util.Observable;
 import java.util.Observer;
 
+import comunicacion_serial.SerialCom;
 import processing.core.PApplet;
 
 public class Logica implements Observer {
-	private LinkedList<Cabra> cabras = new LinkedList<Cabra>();
+
 	private int valor_fotoCelda;
 	private int r = 0;
-	PApplet p;
+	private PApplet p;
+	private Potrero potrero;
 
 	public Logica(PApplet p) {
 		this.p = p;
 		// SerialCom.getRef().addObserver(this);
 		// new Thread(SerialCom.getRef()).start();
 		this.valor_fotoCelda = 0;
-
-		for (int i = 0; i < 50; i++) {
-			Cabra b = new Cabra(p);
-			b.start();
-			cabras.add(b);
-		}
+		potrero = new Potrero(p);
 
 	}
 
@@ -34,15 +31,12 @@ public class Logica implements Observer {
 
 	public void ejecutar(PApplet p) {
 		// TODO Auto-generated method stub
-		System.out.println("Valor de foto celda: " + valor_fotoCelda);
-		p.background(0);
+		// System.out.println("Valor de foto celda: " + valor_fotoCelda);
+		p.background(230, 220, 200);
 		r = (int) PApplet.map(valor_fotoCelda, 0, 1023, 0, 300);
 		p.ellipse(p.width / 2, p.height / 2, r, r);
 
-		for (Cabra m : cabras) {
-			m.display();
-
-		}
+		potrero.ejecutar();
 
 	}
 
--- src/hci_dos_experimento_uno/Potrero.java
+++ src/hci_dos_experimento_uno/Potrero.java
@@ -7,35 +7,91 @@ import processing.core.PApplet;
 public class Potrero {
 
 	private PApplet p;
+	private LinkedList<Cabra> cabras = new LinkedList<Cabra>();
 	private LinkedList<Pasto> pastos = new LinkedList<Pasto>();
-
-	private int var = 10000;
+	private int num_pastos;
+	private int energia = 10000;
+	int menos = 1;
 
 	public Potrero(PApplet p) {
 		this.p = p;
+		/*-------------------Cabras---------------------*/
+		for (int i = 0; i < 50; i++) {
+			Cabra b = new Cabra(p);
+			b.start();
+			cabras.add(b);
+		}
+		/*-------------------Cabras---------------------*/
+		/*-------------Pastos-------------*/
+		num_pastos = (int) PApplet.map(energia, 0, 10000, 0, 800);
+		for (int i = 0; i < num_pastos; i++) {
+			int x = (int) p.random(0, p.width);
+			int y = (int) p.random(0, p.height);
+			int radio = (int) p.random(10, 30);
+			pastos.add(new Pasto(x, y, radio));
+
+		}
+		/*-------------Pastos-------------*/
 
 	}
 
-	private class Pasto extends Thread {
-		private int x, y, radio, r, g, b, t;
+	public void ejecutar() {
+		for (Pasto pasto : pastos) {
+			pasto.pintar();
+			pasto.viento();
+		}
+		for (Cabra m : cabras) {
+			m.display();
+		}
+
+	}
+
+	private class Pasto {
+
+		private int t, x, y, radio, r, g, b;
+		private double onda, indice, amplitud, suma;
+		private boolean muerto;
 
 		public Pasto(int x, int y, int radio) {
 			this.x = x;
 			this.y = y;
 			this.radio = radio;
-			r = 110;
-			g = 210;
-			b = 40;
+			r = 35;
+			g = 200;
+			b = 30;
+			t = 100;
+			amplitud = p.random(3, 10);
+			suma = p.random((float) 0.01, (float) 0.1);
+			indice = 0.1;
+			muerto = false;
 		}
 
-		public void setTransparencia(int t) {
-			// TODO Auto-generated method stub
+		public void desvanecer(int t) {
 			this.t = t;
+		}
+
+		public void agotar(int gasto) {
+			if (muerto == false) {
+				this.radio -= gasto;
+				if (radio <= 5) {
+					muerto = true;
+				}
+			}
+		}
+
+		public boolean isMuerto() {
+			return muerto;
+		}
+
+		public void viento() {
+			indice += suma;
+			onda = (float) PApplet.sin((float) indice) * amplitud;
 		}
 
 		public void pintar() {
 			p.fill(r, g, b, t);
-			p.ellipse(x, y, radio, radio);
+			p.noStroke();
+			p.ellipse((float) (x + onda), y, radio, radio);
 		}
 
 	}
