f0133bb94339430fad3b0708558b34a8e34be585
Jose David G <josedavidgm1995@gmail.com>
2017-08-12 13:29:20 -0500

    add musica

--- src/hci_dos_experimento_uno/Logica.java
+++ src/hci_dos_experimento_uno/Logica.java
@@ -39,5 +39,7 @@ public class Logica implements Observer {
 		potrero.ejecutar();
 
 	}
+	
+	
 
 }
--- src/hci_dos_experimento_uno/Main.java
+++ src/hci_dos_experimento_uno/Main.java
@@ -1,10 +1,12 @@
 package hci_dos_experimento_uno;
 
+import musica.Reproductor;
 import processing.core.*;
 
 public class Main extends PApplet {
 
 	private Logica logica;
+	private Reproductor r;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -13,19 +15,24 @@ public class Main extends PApplet {
 
 	@Override
 	public void settings() {
-		size(700, 700);
+		size(700, 700,P3D);
 	}
 
 	@Override
 	public void setup() {
 		logica = new Logica(this);
-
+		r = new Reproductor(this);
 	}
 
 	@Override
 	public void draw() {
 		logica.ejecutar(this);
 
+	}
+
+	@Override
+	public void mousePressed() {
+		r.reproducir_sample(0);
 	}
 
 }
